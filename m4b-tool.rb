f2d0114c24a71b959b2d3edcddb64b01788a751f
andreas <andreas.schroden@gmail.com>
2018-12-28 02:17:57 +0100

    improved m4b-tool formula

--- m4b-tool.rb
+++ m4b-tool.rb
@@ -1,15 +1,13 @@
-
+# brew tap sandreas/tap
 class M4bTool < Formula
   desc "m4b-tool is a command line utility to merge, split and chapterize audiobook files such as mp3, ogg, flac, m4a or m4b"
   homepage "m4b-tool.fynder.de"
   url "https://github.com/sandreas/m4b-tool/releases/download/v.0.3.1/m4b-tool.tar.gz"
   sha256 "91c0ed60fa240c5cd4faf642d5352b3bdde48bde6af746debff734601c47a393"
 
-  depends_on "php@7.2"
-  depends_on "mp4v2"
-  depends_on :formula => 'ffmpeg',
-             :version => '>= 4.0.0',
-             :options => %w{ --with-faac --with-fdk-aac --with-sdl2 --with-freetype --with-libass --with-libquvi --with-libvorbis --with-libvpx --with-opus --with-x265 }             
+  depends_on "php" :build
+  depends_on "mp4v2" :build
+  depends_on 'ffmpeg' :build
   depends_on "fdk-aac-encoder" => :optional
   
   def install

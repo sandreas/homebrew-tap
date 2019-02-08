416373ecb202b1ae8a0afc654dcabcf736e2f418
andreas <andreas.schroden@gmail.com>
2019-02-08 01:20:40 +0100

    fixed #23 - added --no-conversion flag

--- m4b-tool.rb
+++ m4b-tool.rb
@@ -3,8 +3,8 @@
 class M4bTool < Formula
   desc "m4b-tool is a command line utility to merge, split and chapterize audiobook files such as mp3, ogg, flac, m4a or m4b"
   homepage "m4b-tool.fynder.de"
-  url "https://github.com/sandreas/m4b-tool/releases/download/v.0.3.1/m4b-tool.tar.gz"
-  sha256 "e649af4bd499f50fd4aa2cb8f9f367a2feb759e748f8d7413939be06d196b731"
+  url "https://github.com/sandreas/m4b-tool/releases/download/v.0.3.2/m4b-tool.tar.gz"
+  sha256 "f1985bd46dbc3837212dcd3fb5eb0588a8b92e6073daac27ac233048beab02c8"
 
   depends_on "php"
   depends_on "mp4v2"
@@ -16,7 +16,7 @@ class M4bTool < Formula
   end
   
   test do
-    assert_equal "m4b-tool v.0.3.1", shell_output("#{bin}/m4b-tool --version").chomp
+    assert_equal "m4b-tool v.0.3.2", shell_output("#{bin}/m4b-tool --version").chomp
   end
 
 end

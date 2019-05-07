c099550690e850d3677f3ae11662ad061251d692
andreas <andreas.schroden@gmail.com>
2019-05-07 22:10:00 +0200

    Prepared release 0.4.1

--- Formula/m4b-tool.rb
+++ Formula/m4b-tool.rb
@@ -3,8 +3,8 @@
 class M4bTool < Formula
   desc "m4b-tool is a command line utility to merge, split and chapterize audiobook files such as mp3, ogg, flac, m4a or m4b"
   homepage "m4b-tool.fynder.de"
-  url "https://github.com/sandreas/m4b-tool/releases/download/v.0.4.0/m4b-tool.tar.gz"
-  sha256 "6cb0d27e3b6c518d7a22722aac3d18b1a363802491500fa0e86c017820d261e2"
+  url "https://github.com/sandreas/m4b-tool/releases/download/v.0.4.1/m4b-tool.tar.gz"
+  sha256 "ceb3f14619e064504e2a99f5f3ac08d9447de32a1232af7a44b3c4241c722f9c"
 
   depends_on "php"
   depends_on "mp4v2"
@@ -16,7 +16,7 @@ class M4bTool < Formula
   end
   
   test do
-    assert_equal "m4b-tool v.0.4.0", shell_output("#{bin}/m4b-tool --version").chomp
+    assert_equal "m4b-tool v.0.4.1", shell_output("#{bin}/m4b-tool --version").chomp
   end
 
 end

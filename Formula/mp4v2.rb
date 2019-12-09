f0fafa47f4c87c3155b50c50527f15ae724de43f
andreas <andreas.schroden@gmail.com>
2019-12-09 18:58:21 +0100

    next mp4v2 test

--- Formula/mp4v2.rb
+++ Formula/mp4v2.rb
@@ -1,12 +1,10 @@
-class Mp4v2 < Formula
+# brew tap sandreas/tap
+# brew install m4b-tool
+class M4bTool < Formula
   desc "Read, create, and modify MP4 files"
   homepage "https://github.com/sandreas/m4b-tool"
   url "https://github.com/sandreas/mp4v2/tarball/master"
   sha256 "7fdb74872b52aa21f284304962d30dc9b15cd06849295ae42ae867746bb5ff64"
-
-
-  conflicts_with "bento4",
-    :because => "both install `mp4extract` and `mp4info` binaries"
 
   def install
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
@@ -18,4 +16,5 @@ class Mp4v2 < Formula
   test do
     assert_match "mp4tags - MP4v2 -r", shell_output("#{bin}/mp4art --version")
   end
+
 end

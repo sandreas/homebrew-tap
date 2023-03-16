a83c7d952c4a51fe0f4f6a4b5151ce6a996ffa61
Abderahmane Bouziane <bobmatt911@gmail.com>
2023-03-16 12:36:28 -0400

    hash 2

--- Formula/m4b-tool.rb
+++ Formula/m4b-tool.rb
@@ -5,7 +5,7 @@ class M4bTool < Formula
   desc "m4b-tool is a command line utility to merge, split and chapterize audiobook files such as mp3, ogg, flac, m4a or m4b"
   homepage "https://github.com/sandreas/m4b-tool"
   url "https://github.com/sandreas/m4b-tool/files/10728378/m4b-tool.tar.gz"
-  sha256 "181fb3e332849ea704ef860aaabaf9dcab2fdab298ffba99e66d31efa18b02f"
+  sha256 "181fb3e332849ea704ef860aaabaf9dcab2fdab298ffba99e66d31efa18b02f2"
 
   depends_on "php"
   depends_on "sandreas/tap/mp4v2"

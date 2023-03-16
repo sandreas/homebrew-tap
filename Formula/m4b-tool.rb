65a124d46788171e42f47e8b58ecf7172971d4d5
Abderahmane Bouziane <bobmatt911@gmail.com>
2023-03-16 12:30:18 -0400

    Update m4b-tool.rb

--- Formula/m4b-tool.rb
+++ Formula/m4b-tool.rb
@@ -1,10 +1,10 @@
 # brew tap sandreas/tap
 # brew install m4b-tool
 class M4bTool < Formula
-  version "0.4.2"
+  version "pre-release"
   desc "m4b-tool is a command line utility to merge, split and chapterize audiobook files such as mp3, ogg, flac, m4a or m4b"
   homepage "https://github.com/sandreas/m4b-tool"
-  url "https://github.com/sandreas/m4b-tool/releases/download/v.#{version}/m4b-tool.tar.gz"
+  url "https://github.com/sandreas/m4b-tool/files/10728378/m4b-tool.tar.gz"
   sha256 "0ec79733770379dbd11ea9d76fd03d62d8aacca8e66730779f7caaa92f577ba7"
 
   depends_on "php"
